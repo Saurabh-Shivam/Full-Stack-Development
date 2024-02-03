7fa42212f590a7faca4af061559ac5a86b6e14e6
Saurabh_Shivam <60539011+Saurabh-Shivam@users.noreply.github.com>
2024-02-03 11:46:01 +0530

    CSS Folder Added

--- JavaScript/JavaScript Exercises/Exercise - 1/Exercise2.js
+++
@@ -1,8 +0,0 @@
-// Write a function that takes a string as an argument and returns its length.
-
-function getStringLength(str) {
-  return str.length;
-}
-
-const str = "Saurabh Shivam";
-console.log(`String - ${str} \nLength : ${getStringLength(str)}`);
--- JavaScript/JavaScript Exercises/Exercise - 2/tempCodeRunnerFile.js
+++
@@ -1,2 +0,0 @@
-const maxObj1 = findMaxByProperty(arr1, "price");
-console.log(maxObj1); // { name: 'orange', price: 3 }
