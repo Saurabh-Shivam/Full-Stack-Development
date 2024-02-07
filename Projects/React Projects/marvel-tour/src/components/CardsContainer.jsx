cf68a82952042e7306cba61cccd764e9d29f4d7a
Saurabh_Shivam <60539011+Saurabh-Shivam@users.noreply.github.com>
2024-02-07 21:42:03 +0530

    multiple cards view using map ufunction success

--- Projects/React Projects/marvel-tour/src/components/CardsContainer.jsx
+++ Projects/React Projects/marvel-tour/src/components/CardsContainer.jsx
@@ -4,21 +4,36 @@ import Card from "./Card";
 const CardsContainer = ({ cardValue }) => {
   console.log("Inside cardsContainer");
   console.log(cardValue);
-  console.log(cardValue[1]);
+  // console.log(cardValue[1]);
   // console.log(cardValue[1]?.urls[1]?.url);
-  let name = cardValue[1]?.name;
-  let id = cardValue[1]?.id;
-  let desc = cardValue[1]?.description;
-  let comics = cardValue[1]?.comics?.available;
-  let events = cardValue[1]?.comics?.available;
-  let stories = cardValue[1]?.comics?.available;
-  let series = cardValue[1]?.comics?.available;
-  let image = `${cardValue[1]?.thumbnail?.path}.${cardValue[1]?.thumbnail?.extension}`;
-  let comicLink = cardValue[1]?.urls[1]?.url;
+  // let name = cardValue[1]?.name;
+  // let id = cardValue[1]?.id;
+  // let desc = cardValue[1]?.description;
+  // let comics = cardValue[1]?.comics?.available;
+  // let events = cardValue[1]?.events?.available;
+  // let stories = cardValue[1]?.stories?.available;
+  // let series = cardValue[1]?.series?.available;
+  // let image = `${cardValue[1]?.thumbnail?.path}.${cardValue[1]?.thumbnail?.extension}`;
+  // let comicLink = cardValue[1]?.urls[1]?.url;
   // console.log(image);
   return (
     <div>
-      <Card
+      {cardValue.map((card, index) => (
+        <Card
+          key={index}
+          name={card.name}
+          id={card.id}
+          desc={card.description}
+          comics={card.comics.available}
+          events={card.events.available}
+          stories={card.stories.available}
+          series={card.series.available}
+          image={`${card.thumbnail.path}.${card.thumbnail.extension}`}
+          comicLink={card.urls.find((url) => url.type === "comiclink").url}
+        />
+      ))}
+
+      {/* <Card
         name={name}
         id={id}
         desc={desc}
@@ -28,7 +43,7 @@ const CardsContainer = ({ cardValue }) => {
         series={series}
         image={image}
         comicLink={comicLink}
-      />
+      /> */}
     </div>
   );
 };
