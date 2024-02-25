94c56864770d917b909e43c45be872312d81e57c
Saurabh_Shivam <60539011+Saurabh-Shivam@users.noreply.github.com>
2024-02-25 12:47:33 +0530

    send mail with the help of nodemailer

--- Backend/File_Upload/models/File.js
+++ Backend/File_Upload/models/File.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
+// creating schema
 const fileSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -17,5 +20,35 @@ const fileSchema = new mongoose.Schema({
   },
 });
 
+// post middleware
+// doc ->  the entry which is created in our database is referred to as doc here
+fileSchema.post("save", async function (doc) {
+  try {
+    console.log("Doc -> ", doc);
+    // TODO: Shift this configuration under /config folder
+    // creating transporter -> refer to nodemailer documentation
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+
+    // send mail -> refer to nodemailer documentation
+    let info = await transporter.sendMail({
+      from: `FileUploader Project - Saurabh`,
+      to: doc.email,
+      subject: "New File Uploaded on Cloudinary",
+      html: `<h2>Hello Jee</h2> <p>File Uploaded View here: <a href="${doc.imageUrl}">${doc.imageUrl}</a></p>`,
+    });
+
+    console.log(info);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// creating and exporting model
 const File = mongoose.model("File", fileSchema);
 module.exports = File;
